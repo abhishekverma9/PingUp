dab41d889a999995188b2b3eba3f3efec9ac7cf4
Abhishek verma <iamabhishekk1@gmail.com>
2026-06-05 15:04:41 +0530

    delete vercel.json from backend

--- backend/server.js
+++ backend/server.js
@@ -40,7 +40,7 @@ const server = http.createServer(app);
 // ========================
 export const io = new Server(server, {
   cors: {
-    origin: process.env.FRONTEND_URL || "*", 
+    origin: process.env.FRONTEND_URL || "http://localhost:5173", 
     methods: ["GET", "POST"]
   }
 });
@@ -158,7 +158,7 @@ app.use(express.json({ limit: "5mb" }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL, 
+    origin: process.env.FRONTEND_URL || "http://localhost:5173", 
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization'] 
   })
